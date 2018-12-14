f58826d7e2dfd6cfc9067410760b1a3c5915cdd8
TuzhMS <tuzhilkin.michael.1995@yandex.ru>
2018-12-15 01:27:56 +0300

    MergingPeekingIncreasingIterator is ready and test good

--- src/ru/mail/polis/collections/iterator/todo/MergingPeekingIncreasingIterator.java
+++ src/ru/mail/polis/collections/iterator/todo/MergingPeekingIncreasingIterator.java
@@ -1,6 +1,11 @@
 package ru.mail.polis.collections.iterator.todo;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.PriorityQueue;
+
+import ru.mail.polis.collections.iterator.IPeekingIterator;
+import ru.mail.polis.collections.list.todo.ArrayPriorityQueueSimple;
 
 /**
  * Итератор возвращающий последовательность последовательностей элементов возрастающих итераторов в порядке возрастания
@@ -15,34 +20,43 @@ import java.util.Iterator;
  */
 public class MergingPeekingIncreasingIterator implements Iterator<Integer> {
 
+    PriorityQueue<IntegerIncreasingSequencePeekingIterator> priorityQueue;
+
     /**
      * Creates a {@code MergingPeekingIncreasingIterator} containing the inside all elements of this specified iterators.
-     *
+     * <p>
      * Complexity = O(n)
      *
      * @param iterators the iterators whose are to be placed into this merging peeking increasing iterator
      */
     public MergingPeekingIncreasingIterator(IntegerIncreasingSequencePeekingIterator... iterators) {
-        //todo: do some stuff with iterators
+        priorityQueue = new PriorityQueue<>(IntegerIncreasingSequencePeekingIterator::compareTo);
+        for (IntegerIncreasingSequencePeekingIterator iterator : iterators) {
+            priorityQueue.add(iterator);
+        }
     }
 
     /**
      * Returns {@code true} if the iteration has more elements.
      * (In other words, returns {@code true} if {@link #next} would
      * return an element rather than throwing an exception.)
-     *
+     * <p>
      * Complexity = O(1)
      *
      * @return {@code true} if the iteration has more elements
      */
     @Override
     public boolean hasNext() {
-        throw new UnsupportedOperationException("todo: implement this");
+        try {
+            return priorityQueue.element().hasNext();
+        } catch (NoSuchElementException e) {
+            return false;
+        }
     }
 
     /**
      * Returns the next element in the iteration.
-     *
+     * <p>
      * Complexity = O(log(n))
      *
      * @return the next element in the iteration
@@ -50,6 +64,15 @@ public class MergingPeekingIncreasingIterator implements Iterator<Integer> {
      */
     @Override
     public Integer next() {
-        throw new UnsupportedOperationException("todo: implement this");
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        IntegerIncreasingSequencePeekingIterator iterator = priorityQueue.remove();
+        Integer i = iterator.next();
+        if (iterator.hasNext()) {
+            priorityQueue.add(iterator);
+        }
+        System.out.println(i);
+        return i;
     }
 }
