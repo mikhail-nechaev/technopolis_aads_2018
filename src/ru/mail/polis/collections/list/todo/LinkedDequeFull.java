bd285ca5a608e8458e5669f6edd8571a4aa30f23
Denispok <shakhover@yandex.ru>
2018-12-19 13:08:49 +0300

    rebase

--- src/ru/mail/polis/collections/list/todo/LinkedDequeFull.java
+++ src/ru/mail/polis/collections/list/todo/LinkedDequeFull.java
@@ -2,6 +2,6 @@ package ru.mail.polis.collections.list.todo;
 
 import java.util.Deque;
 
-public abstract class LinkedDequeFull<E> extends LinkedDequeSimple<E> implements Deque<E> {
+public abstract class LinkedDequeFull<E> extends LinkedDequeSimple<E> /*implements Deque<E>*/ {
     //todo: remove <abstract> modifier and implement
 }
